194000a30f8aae04ad9db834b8b368833d2eca0a
kanishk <kanishkupadhyay5@gmail.com>
2021-06-02 18:42:25 +0530

    add invalid hex functionality

--- src/app/left-slide/left-slide.component.ts
+++ src/app/left-slide/left-slide.component.ts
@@ -7,13 +7,14 @@ import { DesignService } from '../service/design.service';
   styleUrls: ['./left-slide.component.css']
 })
 export class LeftSlideComponent {
-  invalid:boolean=false
+  invalid: boolean = false;
+  pattern = /^#([\da-f]{3}){1,2}$|^#([\da-f]{4}){1,2}$/i;
+
   constructor(private service: DesignService) { }
 
-  onChange(colorName) {
-    this.service.color.next(`#${colorName.value}`)
-    if (colorName.value == '') {
-      this.service.color.next('#0000cc')
-    }
+  onChange({ value }) {
+    const colorCode = `#${value}`;
+    this.invalid = !this.pattern.test(colorCode);
+    this.service.color.next(colorCode)
   }
 }
